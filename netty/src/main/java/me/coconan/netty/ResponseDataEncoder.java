de0eb7a1538a844643622358576d14fe8f1969af
coconan <coconan8@gmail.com>
2022-05-31 18:58:38 +0800

    hack netty

---
+++ netty/src/main/java/me/coconan/netty/ResponseDataEncoder.java
@@ -0,0 +1,18 @@
+package me.coconan.netty;
+
+import java.nio.charset.Charset;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+public class ResponseDataEncoder extends MessageToByteEncoder<RequestData> {
+    
+    private final Charset charset = Charset.forName("UTF-8");
+
+    protected void encode(ChannelHandlerContext ctx, RequestData msg, ByteBuf out) throws Exception {
+        out.writeInt(msg.getIntValue());
+        out.writeInt(msg.getStringValue().length());
+        out.writeCharSequence(msg.getStringValue(), charset);
+    }
+}
