019c22369083561746b322cc4ad269cd44b2cfea
coconan <coconan.by@gmail.com>
2023-04-11 23:30:54 +0800

    hack lettuce

--- lettuce/src/main/java/me/coconan/LettuceExample.java
+++ lettuce/src/main/java/me/coconan/LettuceExample.java
@@ -2,7 +2,11 @@ package me.coconan;
 
 import io.lettuce.core.*;
 import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.async.RedisAsyncCommands;
 import io.lettuce.core.api.sync.RedisCommands;
+
+import java.util.concurrent.ExecutionException;
+
 
 public class LettuceExample {
     public static void main(String[] args) {
@@ -12,6 +16,14 @@ public class LettuceExample {
 
         syncCommands.set("key", "Hello, Redis!");
         System.out.println(syncCommands.get("key"));
+
+        RedisAsyncCommands<String, String> asyncCommands = connection.async();
+        RedisFuture<String> future = asyncCommands.get("key");
+        try {
+            System.out.println(future.get());
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
 
         connection.close();
         redisClient.shutdown();
